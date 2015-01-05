1fc87a162aeb95c88715252c570f831173f5ca41
TonightFeeling <little_more@outlook.com>
2015-01-05 20:07:32 +0800

    Ants
    第一天

---
+++ Draw/Draw/Ants.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw
+{
+    class Ants
+    {
+        protected enum Directions
+        {
+            Left,
+            Right
+        };
+
+        private const int L = 1000000; // 杆的长度
+
+        public static void Solve(int l, List<int> ants)
+        {
+            //最短时间是所有蚂蚁都超最近的断点走去，这样不会发生蚂蚁相遇的情况
+            int minTime = 0;
+            foreach (var ant in ants)
+            {
+                minTime += Math.Max(minTime, Math.Min(ant, l - ant));
+            }
+
+            int maxTime = 0;
+            foreach (var ant in ants)
+            {
+                maxTime +=Math.Max(maxTime, Math.Max(ant, l - ant));
+            }
+
+        }
+
+    }
+
+}
---
+++ simple1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw
+{
+    class Program
+    {
+        private const int LengthMax = 100000; // 棍子的长度限制
+        private const int Count = 100; //数组的个数
+
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            List<int> edges = new List<int>(Count);
+            for (int i = 0; i < Count; i++)
+            {
+                edges.Add(rand.Next(1, LengthMax));
+            }
+
+            Solve(edges);
+        }
+
+        private static void Solve(List<int> edges)
+        {
+            int maxLength = 0;
+            for (int i = 0; i < edges.Count; i++)
+                for (int j = i + 1; j < edges.Count; j++)
+                    for (int k = j + 1; k < edges.Count; k++)
+                    {
+                        int a = edges[i];
+                        int b = edges[j];
+                        int c = edges[k];
+
+                        if (a + b > c && b + c > a && a + c > b)
+                        {
+                            maxLength = Math.Max(maxLength, a + b + b);
+                        }
+                    }
+            Console.WriteLine(maxLength);
+        }
+    }
+}
